a37feae62c6e74cb6858de83d5716cab728c33d0
thoq <tristanshaw000@gmail.com>
2025-04-28 20:01:48 -0400

    add cli wrapper

--- src/java_src/src/main/resources/src/routes/+page.svelte
+++ src/java_src/src/main/resources/src/routes/+page.svelte
@@ -1,4 +1,7 @@
-<h1>Welcome to Jauri!</h1>
+<div>
+    <h1>Welcome to Jauri!</h1>
+    <p>To develop with Jauri, begin with the <a href="https://jauri.cirkl.ai">Jauri CLI</a></p>
+</div>
 
 <style>
 
