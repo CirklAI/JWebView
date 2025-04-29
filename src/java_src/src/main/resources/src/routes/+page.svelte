0c45b0d178ada3e71d0f606d7f75f80d3e5b9193
thoq <tristanshaw000@gmail.com>
2025-04-28 20:34:57 -0400

    update demo

--- src/java_src/src/main/resources/src/routes/+page.svelte
+++ src/java_src/src/main/resources/src/routes/+page.svelte
@@ -1,8 +1,7 @@
-<div>
+<div style="display: flex; flex-direction: column; align-items: center; justify-content: center; text-align: center">
     <h1>Welcome to Jauri!</h1>
-    <p>To develop with Jauri, begin with the <a href="https://jauri.cirkl.ai">Jauri CLI</a></p>
+    <p>
+        To develop with Jauri, begin with the
+        <a style="color: #ff78f9" href="https://jauri.cirkl.ai">Jauri CLI</a>
+    </p>
 </div>
-
-<style>
-
-</style>
